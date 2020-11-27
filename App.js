a12ce70a0fc5aa5fc8a91dcd698b3482cb150d35
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2020-11-27 16:38:15 -0600

    Se está dando diseño touch al botón

--- App.js
+++ App.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import Registar from './src/components/Botones/Registrar'
 
+const presionarBoton = (nombre) => {
+  console.log(`Se presiono ${nombre}`);
+}
+
 export default function App() {
   return (
     <>
       <Text>Este es el botón</Text>
       <View >
-        <Registar />
+        <Registar nombre={'REGISTRAR'} sePresiona={presionarBoton} />
       </View>
     </>
   );
-
 }
-
--- src/components/Botones/Registrar.js
+++ src/components/Botones/Registrar.js
@@ -1,22 +1,47 @@
-import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native'
 
-export default function Registrar() {
+export default function Registrar(props) {
+    const { nombre, sePresiona } = props;
+    const [pressed, setPressed] = useState(false);
+
     return (
-        <View style={{width: '100%', alignItems: 'center'}}>
-            <TouchableOpacity>
-                <View style={styles.viewButton}>
-                    <Text style={styles.text} >REGISTRAR</Text>
+        <View style={{ width: '100%', alignItems: 'center' }}>
+            <TouchableHighlight
+                activeOpacity={1}
+                onHideUnderlay={() => { setPressed(false) }} // Cuando el botón NO es presionado
+                onShowUnderlay={() => { setPressed(true) }} // Cuando el botón es presionado
+                onPress={() => sePresiona(nombre)}
+            >
+                <View style={[styles.viewButton, pressed ? { backgroundColor: '#FFF' } : { backgroundColor: '#1D4293' }]}>
+                    <Text style={[styles.text, pressed ? { color: '#1D4293' } : { color: '#FFF' }]} >{nombre}</Text>
                 </View>
-            </TouchableOpacity>
+            </TouchableHighlight>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    // viewButton: {
+    //     alignContent: 'center',
+    //     backgroundColor: '#1D4293',
+    //     borderWidth: 1,
+    //     borderColor: '#1D4293',
+    //     borderRadius: 8,
+    //     paddingVertical: 10,
+    //     paddingHorizontal: 10,
+    // },
+
+    // text: {
+    //     fontSize: 15.3737,
+    //     color: '#FFF',
+    //     textAlign: 'center',
+    //     fontFamily: 'LatoRegular',
+    // },
+
     viewButton: {
         alignContent: 'center',
-        backgroundColor: '#1D4293',
+        // backgroundColor: '#FFF',
         borderWidth: 1,
         borderColor: '#1D4293',
         borderRadius: 8,
@@ -26,7 +51,7 @@ const styles = StyleSheet.create({
 
     text: {
         fontSize: 15.3737,
-        color: '#FFF',
+        //color: '#1D4293',
         textAlign: 'center',
         fontFamily: 'LatoRegular',
     },
