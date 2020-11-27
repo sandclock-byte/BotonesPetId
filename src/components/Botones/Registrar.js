dd106087ed6af48fbad7f22b690e317fb1987204
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2020-11-27 16:53:20 -0600

    Se está diseñando componente botón

--- src/components/Botones/Registrar.js
+++ src/components/Botones/Registrar.js
@@ -8,7 +8,7 @@ export default function Registrar(props) {
     return (
         <View style={{ width: '100%', alignItems: 'center' }}>
             <TouchableHighlight
-                activeOpacity={1}
+                underlayColor={'white'}
                 onHideUnderlay={() => { setPressed(false) }} // Cuando el botón NO es presionado
                 onShowUnderlay={() => { setPressed(true) }} // Cuando el botón es presionado
                 onPress={() => sePresiona(nombre)}
@@ -22,36 +22,18 @@ export default function Registrar(props) {
 }
 
 const styles = StyleSheet.create({
-    // viewButton: {
-    //     alignContent: 'center',
-    //     backgroundColor: '#1D4293',
-    //     borderWidth: 1,
-    //     borderColor: '#1D4293',
-    //     borderRadius: 8,
-    //     paddingVertical: 10,
-    //     paddingHorizontal: 10,
-    // },
-
-    // text: {
-    //     fontSize: 15.3737,
-    //     color: '#FFF',
-    //     textAlign: 'center',
-    //     fontFamily: 'LatoRegular',
-    // },
 
     viewButton: {
         alignContent: 'center',
-        // backgroundColor: '#FFF',
         borderWidth: 1,
         borderColor: '#1D4293',
         borderRadius: 8,
-        paddingVertical: 10,
-        paddingHorizontal: 10,
+        paddingVertical: 12,
+        paddingHorizontal: 12,
     },
 
     text: {
         fontSize: 15.3737,
-        //color: '#1D4293',
         textAlign: 'center',
         fontFamily: 'LatoRegular',
     },
