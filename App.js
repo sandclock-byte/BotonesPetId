625a190fb3048eedececed275116ab8101f250a1
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2020-11-30 12:03:57 -0600

    Se agrego state para probar diseño botonAcceso

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, Text, TextInput } from 'react-native'
 import BotonAzul from './src/components/Botones/BotonAzul'
 import BotonAcceso from './src/components/Botones/BotonAcceso'
 
@@ -9,13 +9,47 @@ const presionarBoton = (nombre) => {
 }
 
 export default function App() {
+
+  const [formData, setFormData] = useState('');
+  const [isAvailable, setIsAvailable] = useState(false);
+
+  useEffect(() => {
+    formData != '' ? setIsAvailable(true) : setIsAvailable(false);
+  }, [formData])
+
   return (
     <>
       <Text>Este es el botón</Text>
       <View >
-        {/* <BotonAzul title={'REGISTRAR'} onPress={() => presionarBoton('REGISTRAR')} /> */}
-        <BotonAcceso title={'ACCESO'} onPress={() => presionarBoton('REGISTRAR')} />
+        <TextInput
+          style={styles.input}
+          placeholder='Contraseña'
+          placeholderTextColor='#969696'
+          secureTextEntry={true}
+          onChange={(e) => setFormData(e.nativeEvent.text.trim())}
+        />
+        <BotonAcceso
+          title={'ACCESO'}
+          onPress={() => presionarBoton('ACCESO')}
+          available={isAvailable}
+        />
       </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+
+  input: {
+    height: 50,
+    color: '#FFF',
+    width: '80%',
+    marginBottom: 25,
+    backgroundColor: '#1E3040',
+    paddingHorizontal: 20,
+    borderRadius: 50,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#1E3040'
+  },
+});
